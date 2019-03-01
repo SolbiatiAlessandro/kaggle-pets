9fb4406837707775124d03d2af50e7b90a608d84
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-03-02 00:37:33 +0100

    [LOW] added verbose option to model

--- models/KNN/knnModel.py
+++ models/KNN/knnModel.py
@@ -17,7 +17,7 @@ class PredictiveModel(object):
         self.predictions = None
         print("{} [{}.__init__] initialized succesfully".format(ctime(), self.name))
 
-    def validation(self, X, Y, method=1):
+    def validation(self, X, Y, method=1, verbose=False):
         """
         validation method, you can choose between different validation strategies
 
@@ -33,7 +33,7 @@ class PredictiveModel(object):
         NOTE:
         https://www.youtube.com/watch?v=pA6uXzrDSUs&index=23&list=PLpQWTe-45nxL3bhyAJMEs90KF_gZmuqtm
         """
-        print("{} [{}.validation] start validation method {}".format(ctime(), self.name, method))
+        if verbose: print("{} [{}.validation] start validation method {}".format(ctime(), self.name, method))
         validation_score = 0
 
         # based on method value we choose a model_selection splitclass
@@ -64,19 +64,19 @@ class PredictiveModel(object):
             self.train(train_X, train_Y)
             predictions = self.predict(validation_X)
             score = self.evaluate(validation_Y)
-            print("{} [{}.validation] single score = {} ".format(ctime(), self.name, score))
+            if verbose: print("{} [{}.validation] single score = {} ".format(ctime(), self.name, score))
             validation_score += score
 
         # the total validation score is an average of the single validation scores
         validation_score /= splitclass.get_n_splits(X)
         self.validation_score = validation_score
 
-        print("{} [{}.validation] validation score = {} ".format(ctime(), self.name, validation_score))
-        print("{} [{}.validation] finished validation method {}".format(ctime(), self.name, method))
+        if verbose: print("{} [{}.validation] validation score = {} ".format(ctime(), self.name, validation_score))
+        if verbose: print("{} [{}.validation] finished validation method {}".format(ctime(), self.name, method))
         return validation_score
             
         
-    def train(self, X, Y):
+    def train(self, X, Y, verbose=False):
         """
         train method, feature generation is inside here, data cleaning outside
         
@@ -84,14 +84,14 @@ class PredictiveModel(object):
             X: pandas.DataFrame, shape = (, 24)
             Y: pandas.Series
         """
-        print("{} [{}.train] start training".format(ctime(), self.name))
+        if verbose: print("{} [{}.train] start training".format(ctime(), self.name))
         
         self.model.fit(X, Y)
         
-        print("{} [{}.train] trained succefully".format(ctime(), self.name))
+        if verbose: print("{} [{}.train] trained succefully".format(ctime(), self.name))
 
         
-    def predict(self, X):
+    def predict(self, X, verbose):
         """
         predict method, feature generation is inside here, data cleaning outside
         
@@ -103,15 +103,15 @@ class PredictiveModel(object):
         Raise:
             .not trained
         """
-        print("{} [{}.predict] start predictions".format(ctime(), self.name))
+        if verbose: print("{} [{}.predict] start predictions".format(ctime(), self.name))
 
         predictions = self.model.predict(X)
         self.predictions = predictions
         
-        print("{} [{}.predict] predicted succesfully".format(ctime(), self.name))
+        if verbose: print("{} [{}.predict] predicted succesfully".format(ctime(), self.name))
         return predictions
     
-    def evaluate(self, labels):
+    def evaluate(self, labels, verbose=False):
         """
         evaluate predictions accuracy using competition metric "Quadratic Weighted Kappa"
         more here https://scikit-learn.org/stable/modules/generated/sklearn.metrics.cohen_kappa_score.html
@@ -128,7 +128,7 @@ class PredictiveModel(object):
         Whereas, the least possible score is -1 which is given when the predictions are furthest away from actuals. In our case, consider all actuals were 0's and all predictions were 4's. This would lead to a QWKP score of -1.
         The aim is to get as close to 1 as possible. Generally a score of 0.6+ is considered to be a really good score.
         """
-        print("{} [{}.evaluate] start evaluation".format(ctime(), self.name))
+        if verbose: print("{} [{}.evaluate] start evaluation".format(ctime(), self.name))
         if self.predictions is None:
             raise Exception("{} [{}.evaluate] ERROR model didn't predict, you need to call {}.predict first".format(ctime(), self.name, self.name))
             
@@ -138,5 +138,5 @@ class PredictiveModel(object):
             
         score = metrics.cohen_kappa_score(labels_array, self.predictions)
         
-        print("{} [{}.evaluate] evaluated succesfully".format(ctime(), self.name))
+        if verbose: print("{} [{}.evaluate] evaluated succesfully".format(ctime(), self.name))
         return score
