93763d7e7336e322df73e16112633a14e59bbdcb
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-03-21 21:28:19 -0400

    [catboost] passing tests

--- models/TREES/CATBOOST/tests/test_run.py
+++ models/TREES/CATBOOST/tests/test_run.py
@@ -10,15 +10,15 @@ def getXY():
     loads data from local /data folder and add basic cleaning
     """
     # this is run from /models/KNN from pytest so the the path is correct
-    os.listdir('../../data')
-    assert 'out_breed.csv' in os.listdir('../../data') # this assert breaks if the data is configured uncorrectly
+    os.listdir('../../../data')
+    assert 'out_breed.csv' in os.listdir('../../../data') # this assert breaks if the data is configured uncorrectly
 
-    breeds = pd.read_csv('../../data/out_breed.csv')
-    colors = pd.read_csv('../../data/out_color.csv')
-    states = pd.read_csv('../../data/out_state.csv')
-    train  = pd.read_csv('../../data/out_train.csv')
-    test   = pd.read_csv('../../data/out_test.csv')
-    sub    = pd.read_csv('../../data/out_submission.csv')
+    breeds = pd.read_csv('../../../data/out_breed.csv')
+    colors = pd.read_csv('../../../data/out_color.csv')
+    states = pd.read_csv('../../../data/out_state.csv')
+    train  = pd.read_csv('../../../data/out_train.csv')
+    test   = pd.read_csv('../../../data/out_test.csv')
+    sub    = pd.read_csv('../../../data/out_submission.csv')
 
     X = train.drop(["AdoptionSpeed", "Unnamed: 0", "dataset_type", "Name", "RescuerID", "Description", "PhotoAmt","VideoAmt","PetID"], axis=1)
     Y = train['AdoptionSpeed']
@@ -33,18 +33,26 @@ def test_run():
     this test just runs the load-train-predict workflow
 
     the code is just a script of 
-    85f7ec7c8c0581c347a5b8034139a9ad3a6c3352:./../kNN.ipynb
+    85f7ec7c8c0581c347a5b8034139a9ad3a6c3352../../../kNN.ipynb
     """
     # this sys.path.append are used to import knnModel inside /models/KNN
     sys.path.append(".")
     sys.path.append("../")
-    from lgbmModel import PredictiveModel
+    from catboostModel import PredictiveModel
 
     ###########################################################
     #### this can be used as an example usage of the model ####
     ###########################################################
 
     X, Y = getXY()
+
+    string_cols = ["Unnamed: 0", "dataset_type", "Name", "RescuerID", "Description", "PetID"]
+    categorical_col = ["Type","Gender","Vaccinated","Dewormed","Sterilized","Breed1","Breed2","Color1","Color2","Color3","State"]
+    numerical_col = [col for col in X.columns if col not in string_cols and col not in categorical_col and col != "AdoptionSpeed"]
+    mapping_sizes = [2, 2, 3, 3, 3, 307, 307, 7, 7, 7, 15]
+    cat_features = [i for i in range(len(numerical_col), len(numerical_col)+len(categorical_col))]
+
+    X = pd.concat([X[numerical_col], X[categorical_col]], axis=1)
 
     train_size = int(len(X)*0.8)
 
@@ -55,8 +63,8 @@ def test_run():
     assert train_X.shape[0] == train_Y.shape[0]
     assert validation_X.shape[0] == validation_Y.shape[0]
 
-    model = PredictiveModel("KNN_run_by_pytest")
-    model.train(train_X, train_Y)
+    model = PredictiveModel("catboost_by_pytest")
+    model.train(train_X, train_Y, cat_features)
     predictions = model.predict(validation_X)
     score = model.evaluate(validation_Y)
 
@@ -73,14 +81,21 @@ def test_validation():
     # this sys.path.append are used to import knnModel inside /models/KNN
     sys.path.append(".")
     sys.path.append("../")
-    from lgbmModel import PredictiveModel
+    from catboostModel import PredictiveModel
 
     X, Y = getXY()
-    model = PredictiveModel("KNN_run_by_pytest")
-    assert model.validation(X, Y) > 0
-    assert model.validation(X, Y, method = 1, n_folds = 2) > 0
-    assert model.validation(X, Y, method = 2, n_folds = 2) > 0
-    assert model.validation(X, Y, n_folds = 1) > 0
+    string_cols = ["Unnamed: 0", "dataset_type", "Name", "RescuerID", "Description", "PetID"]
+    categorical_col = ["Type","Gender","Vaccinated","Dewormed","Sterilized","Breed1","Breed2","Color1","Color2","Color3","State"]
+    numerical_col = [col for col in X.columns if col not in string_cols and col not in categorical_col and col != "AdoptionSpeed"]
+    mapping_sizes = [2, 2, 3, 3, 3, 307, 307, 7, 7, 7, 15]
+    cat_features = [i for i in range(len(numerical_col), len(numerical_col)+len(categorical_col))]
+
+    X = pd.concat([X[numerical_col], X[categorical_col]], axis=1)
+    model = PredictiveModel("catboost_by_pytest")
+    assert model.validation(X, Y, cat_features) > 0
+    assert model.validation(X, Y, cat_features, method = 1, n_folds = 2) > 0
+    assert model.validation(X, Y, cat_features, method = 2, n_folds = 2) > 0
+    assert model.validation(X, Y, cat_features, n_folds = 1) > 0
 
     # method 3 is LeaveOneOut: too costly, DEPRECATED
     # assert model.validation(X, Y, method = 3) > 0
